532694a7a7ed23519930857cd89ac8ff9c5944d5
Alex Renoki <rennokki@gmail.com>
2018-07-17 19:26:09 +0300

    Separating traits by usage.

--- src/Traits/CanBlock.php
+++ src/Traits/CanBlock.php
@@ -4,14 +4,6 @@ namespace Rennokki\Befriended\Traits;
 
 trait CanBlock
 {
-    public function blockers($model = null)
-    {
-        return $this->morphToMany(($model) ?: $this->getMorphClass(), 'blockable', 'blockers', 'blockable_id', 'blocker_id')
-                    ->withPivot('blocker_type')
-                    ->wherePivot('blocker_type', ($model) ?: $this->getMorphClass())
-                    ->wherePivot('blockable_type', $this->getMorphClass());
-    }
-
     public function blocking($model = null)
     {
         return $this->morphToMany(($model) ?: $this->getMorphClass(), 'blocker', 'blockers', 'blocker_id', 'blockable_id')
--- src/Traits/CanFollow.php
+++ src/Traits/CanFollow.php
@@ -4,14 +4,6 @@ namespace Rennokki\Befriended\Traits;
 
 trait CanFollow
 {
-    public function followers($model = null)
-    {
-        return $this->morphToMany(($model) ?: $this->getMorphClass(), 'followable', 'followers', 'followable_id', 'follower_id')
-                    ->withPivot('follower_type')
-                    ->wherePivot('follower_type', ($model) ?: $this->getMorphClass())
-                    ->wherePivot('followable_type', $this->getMorphClass());
-    }
-
     public function following($model = null)
     {
         return $this->morphToMany(($model) ?: $this->getMorphClass(), 'follower', 'followers', 'follower_id', 'followable_id')
