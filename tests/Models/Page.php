74f39c72caf06df31f7ddd76fbbd7c4bd9ff221d
Alex Renoki <rennokki@gmail.com>
2018-07-17 19:26:15 +0300

    Updating testing models.

--- tests/Models/Page.php
+++ tests/Models/Page.php
@@ -5,14 +5,16 @@ namespace Rennokki\Befriended\Test\Models;
 use Illuminate\Database\Eloquent\Model;
 use Rennokki\Befriended\Traits\CanBlock;
 use Rennokki\Befriended\Traits\CanFollow;
+use Rennokki\Befriended\Traits\CanBeBlocked;
+use Rennokki\Befriended\Traits\CanBeFollowed;
 use Rennokki\Befriended\Contracts\Blocking;
 use Rennokki\Befriended\Contracts\Following;
-use Rennokki\Befriended\Traits\CanFilterBlocking;
-use Rennokki\Befriended\Traits\CanFilterFollowers;
+use Rennokki\Befriended\Scopes\CanFilterBlocking;
+use Rennokki\Befriended\Scopes\CanFilterFollowers;
 
 class Page extends Model implements Following, Blocking
 {
-    use CanFollow, CanBlock, CanFilterFollowers, CanFilterBlocking;
+    use CanFollow, CanBeFollowed, CanBlock, CanBeBlocked, CanFilterFollowers, CanFilterBlocking;
 
     protected $fillable = [
         'name',
--- tests/Models/User.php
+++ tests/Models/User.php
@@ -4,15 +4,17 @@ namespace Rennokki\Befriended\Test\Models;
 
 use Rennokki\Befriended\Traits\CanBlock;
 use Rennokki\Befriended\Traits\CanFollow;
+use Rennokki\Befriended\Traits\CanBeBlocked;
+use Rennokki\Befriended\Traits\CanBeFollowed;
 use Rennokki\Befriended\Contracts\Blocking;
 use Rennokki\Befriended\Contracts\Following;
-use Rennokki\Befriended\Traits\CanFilterBlocking;
-use Rennokki\Befriended\Traits\CanFilterFollowers;
+use Rennokki\Befriended\Scopes\CanFilterBlocking;
+use Rennokki\Befriended\Scopes\CanFilterFollowers;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements Following, Blocking
 {
-    use CanFollow, CanBlock, CanFilterFollowers, CanFilterBlocking;
+    use CanFollow, CanBeFollowed, CanBlock, CanBeBlocked, CanFilterFollowers, CanFilterBlocking;
 
     protected $fillable = [
         'name', 'email', 'password',
