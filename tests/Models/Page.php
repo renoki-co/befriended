10c4dca21580f7cda2ffd1a27013e82ab7e29337
Alex Renoki <rennokki@gmail.com>
2018-07-20 21:14:15 +0300

    Adding contracts and traits to testing models.

--- tests/Models/Page.php
+++ tests/Models/Page.php
@@ -4,17 +4,20 @@ namespace Rennokki\Befriended\Test\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Rennokki\Befriended\Traits\CanBlock;
+use Rennokki\Befriended\Traits\CanLike;
 use Rennokki\Befriended\Traits\CanFollow;
 use Rennokki\Befriended\Contracts\Blocking;
+use Rennokki\Befriended\Contracts\Liking;
 use Rennokki\Befriended\Contracts\Following;
 use Rennokki\Befriended\Traits\CanBeBlocked;
+use Rennokki\Befriended\Traits\CanBeLiked;
 use Rennokki\Befriended\Traits\CanBeFollowed;
 use Rennokki\Befriended\Scopes\CanFilterBlockedModels;
 use Rennokki\Befriended\Scopes\CanFilterFollowingModels;
 
 class Page extends Model implements Following, Blocking
 {
-    use CanFollow, CanBeFollowed, CanBlock, CanBeBlocked, CanFilterFollowingModels, CanFilterBlockedModels;
+    use CanFollow, CanBeFollowed, CanBlock, CanBeBlocked, CanLike, CanBeLiked, CanFilterFollowingModels, CanFilterBlockedModels;
 
     protected $fillable = [
         'name',
--- tests/Models/User.php
+++ tests/Models/User.php
@@ -3,18 +3,21 @@
 namespace Rennokki\Befriended\Test\Models;
 
 use Rennokki\Befriended\Traits\CanBlock;
+use Rennokki\Befriended\Traits\CanLike;
 use Rennokki\Befriended\Traits\CanFollow;
 use Rennokki\Befriended\Contracts\Blocking;
+use Rennokki\Befriended\Contracts\Liking;
 use Rennokki\Befriended\Contracts\Following;
 use Rennokki\Befriended\Traits\CanBeBlocked;
+use Rennokki\Befriended\Traits\CanBeLiked;
 use Rennokki\Befriended\Traits\CanBeFollowed;
 use Rennokki\Befriended\Scopes\CanFilterBlockedModels;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Rennokki\Befriended\Scopes\CanFilterFollowingModels;
 
-class User extends Authenticatable implements Following, Blocking
+class User extends Authenticatable implements Following, Blocking, Liking
 {
-    use CanFollow, CanBeFollowed, CanBlock, CanBeBlocked, CanFilterFollowingModels, CanFilterBlockedModels;
+    use CanFollow, CanBeFollowed, CanBlock, CanBeBlocked, CanLike, CanBeLiked, CanFilterFollowingModels, CanFilterBlockedModels;
 
     protected $fillable = [
         'name', 'email', 'password',
