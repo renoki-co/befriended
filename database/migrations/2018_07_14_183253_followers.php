a1e7daffb516a24f5b56f8399e25472bef6ddbc2
Alex Renoki <rennokki@gmail.com>
2018-07-20 21:08:32 +0300

    Fixing migrations classes names.

--- database/migrations/2018_07_14_183253_followers.php
+++ database/migrations/2018_07_14_183253_followers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Befriended extends Migration
+class Followers extends Migration
 {
     /**
      * Run the migrations.
