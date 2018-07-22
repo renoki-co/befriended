0df42c38d7e9cd1b7a04cbff41851a62b8a4674c
Alex Renoki <rennokki@gmail.com>
2018-07-22 21:51:34 +0300

    Added models to the config. [skip ci]

--- config/befriended.php
+++ config/befriended.php
@@ -8,6 +8,8 @@ return [
 
     'models' => [
         'follower' => \Rennokki\Befriended\Models\FollowerModel::class,
+        'blocker' => \Rennokki\Befriended\Models\BlockerModel::class,
+        'liker' => \Rennokki\Befriended\Models\LikerModel::class,
     ],
 
 ];
