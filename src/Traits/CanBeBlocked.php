0ca584e4d886ce0ac5c5df8cc0ea3aef5f55c3a3
Jose Jimenez <jimenez1185@gmail.com>
2018-08-11 18:02:05 -0700

    Update CanBeBlocked.php

--- src/Traits/CanBeBlocked.php
+++ src/Traits/CanBeBlocked.php
@@ -15,7 +15,7 @@ trait CanBeBlocked
         return $this->morphToMany(($model) ?: $this->getMorphClass(), 'blockable', 'blockers', 'blockable_id', 'blocker_id')
                     ->withPivot('blocker_type')
                     ->wherePivot('blocker_type', ($model) ?: $this->getMorphClass())
-                    ->wherePivot('blockable_type', $this->getMorphClass());
+                    ->wherePivot('blockable_type', $this->getMorphClass())
                     ->withTimestamps();
     }
 }
