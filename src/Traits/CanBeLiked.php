69cff4d64e306d280f317395cb134eb188baf828
Jose Jimenez <jimenez1185@gmail.com>
2018-08-11 18:02:28 -0700

    Update CanBeLiked.php

--- src/Traits/CanBeLiked.php
+++ src/Traits/CanBeLiked.php
@@ -15,6 +15,7 @@ trait CanBeLiked
         return $this->morphToMany(($model) ?: $this->getMorphClass(), 'likeable', 'likers', 'likeable_id', 'liker_id')
                     ->withPivot('liker_type')
                     ->wherePivot('liker_type', ($model) ?: $this->getMorphClass())
-                    ->wherePivot('likeable_type', $this->getMorphClass());
+                    ->wherePivot('likeable_type', $this->getMorphClass())
+                    ->withTimestamps();
     }
 }
