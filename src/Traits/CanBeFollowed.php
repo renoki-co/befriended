c416217f0b285a52d4360d99b282e521333128d7
Jose Jimenez <jimenez1185@gmail.com>
2018-08-11 18:01:52 -0700

    Update CanBeFollowed.php

--- src/Traits/CanBeFollowed.php
+++ src/Traits/CanBeFollowed.php
@@ -15,6 +15,7 @@ trait CanBeFollowed
         return $this->morphToMany(($model) ?: $this->getMorphClass(), 'followable', 'followers', 'followable_id', 'follower_id')
                     ->withPivot('follower_type')
                     ->wherePivot('follower_type', ($model) ?: $this->getMorphClass())
-                    ->wherePivot('followable_type', $this->getMorphClass());
+                    ->wherePivot('followable_type', $this->getMorphClass())
+                    ->withTimestamps();
     }
 }
