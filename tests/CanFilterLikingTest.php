1e2cad7f444d9371c2224e2f23c9286109737b6a
Alex Renoki <rennokki@gmail.com>
2018-09-16 22:25:15 +0300

    Better code codeverage.

--- tests/CanFilterLikingTest.php
+++ tests/CanFilterLikingTest.php
@@ -21,6 +21,18 @@ class CanFilterLikingTest extends TestCase
         factory(\Rennokki\Befriended\Test\Models\Page::class, 10)->create();
     }
 
+    public function testCanFilterLikes()
+    {
+        $this->assertEquals(Page::likedBy($this->user)->count(), 0);
+        $this->assertEquals(Page::likedBy($this->user2)->count(), 0);
+
+        $this->user->like(Page::find(1));
+        $this->user->like(Page::find(2));
+
+        $this->assertEquals(Page::likedBy($this->user)->count(), 2);
+        $this->assertEquals(Page::likedBy($this->user2)->count(), 0);
+    }
+
     public function testCanFilterNonLikes()
     {
         $this->assertEquals(Page::filterUnlikedFor($this->user)->count(), 10);
