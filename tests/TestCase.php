e80e67fed23d259037c0a0c477efc88e1255f0c2
Babichev Maxim <babichev@mail.tvil.ru>
2019-09-05 15:14:17 +0300

    fix style ci

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Rennokki\Befriended\Test;
 
+use Rennokki\Befriended\Test\Models\Page;
 use Rennokki\Befriended\Test\Models\User;
 use Rennokki\Befriended\Models\LikerModel;
-use Rennokki\Befriended\Test\Models\Page;
 use Rennokki\Befriended\Models\BlockerModel;
 use Rennokki\Befriended\Models\FollowerModel;
 use Orchestra\Testbench\TestCase as Orchestra;
