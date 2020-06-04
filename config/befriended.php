795415f0cfc19deaa9cbbce57380ee27e59cc35c
Alex Renoki <rennokki@gmail.com>
2020-06-04 23:37:23 +0300

    Config details

--- config/befriended.php
+++ config/befriended.php
@@ -3,13 +3,24 @@
 return [
 
     /*
-     * The models for tables.
-     */
+    |--------------------------------------------------------------------------
+    | Models associated with the tables
+    |--------------------------------------------------------------------------
+    |
+    | The following models are the models that interact with the migrated tables.
+    | If you wish to implement your custom logic, please extend the respective class
+    | and replace here the Full Class Name.
+    |
+    */
 
     'models' => [
+
         'follower' => \Rennokki\Befriended\Models\FollowerModel::class,
+
         'blocker' => \Rennokki\Befriended\Models\BlockerModel::class,
+
         'liker' => \Rennokki\Befriended\Models\LikerModel::class,
+
     ],
 
 ];
